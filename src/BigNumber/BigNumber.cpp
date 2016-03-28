0d34e8ab6b82665ae9e5c14b8307a31b30b3cb15
Mark <mail.ormark@gmail.com>
2016-03-28 04:07:40 +0300

    Basic class BigNumber. Print fucntion. Basic Class tests

--- src/BigNumber/BigNumber.cpp
+++ src/BigNumber/BigNumber.cpp
@@ -1,6 +1,22 @@
 #include "BigNumber.h"
+#include <string>
 
-int add(int x, int y)
-{
-    return x + y;
+BigNumber::BigNumber(string numString) {
+    this->sign = 0;
+    if (numString[0] == '-') {
+        this->sign = 1;
+    }
+    this->num = new int[numString.length() - this->sign];
+    this->length = numString.length() - this->sign;
+    for (int i = 0; i < (int) (numString.length() - this->sign); i++) {
+        this->num[i] = (int)(numString[i + this->sign] - '0');
+    }
+}
+
+string BigNumber::print() {
+    string ret = "";
+    for (int i = 0; i < this->length; i++) {
+        ret += std::to_string(this->num[i]);
+    }
+    return (sign) ? '-' + ret : ret;
 }
--- src/BigNumber/BigNumber.h
+++ src/BigNumber/BigNumber.h
@@ -1,7 +1,18 @@
-#ifndef ADD_H
-#define ADD_H
- 
-int add(int x, int y);
- 
- 
+#ifndef BIGNUMBER_H
+#define BIGNUMBER_H
+
+#include <string>
+    using namespace std;
+
+class BigNumber {
+public:
+    int *num;
+    int length;
+    int sign;
+
+public:
+    BigNumber(string);
+    string print();
+};
+
 #endif
