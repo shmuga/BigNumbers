c10aa791d624fb136a3408a549aaa9df952ff647
Mark <mail.ormark@gmail.com>
2016-03-28 03:16:53 +0300

    LATEST MOTHER FUCKER

--- tests/BigNumber/BigNumber.spec.cpp
+++ tests/BigNumber/BigNumber.spec.cpp
@@ -1,9 +1,9 @@
 #include "gtest/gtest.h"
-
+#include "BigNumber/BigNumber.h"
 TEST(FactorialTest, Negative) {
     // This test is named "Negative", and belongs to the "FactorialTest"
     // test case.
-    EXPECT_EQ(1, 1);
+    EXPECT_EQ(1, add(1, 0));
 
     // <TechnicalDetails>
     //
