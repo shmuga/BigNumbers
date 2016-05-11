562e05446bb78ced45609039d8c0ec9662f453a1
Mark <mail.ormark@gmail.com>
2016-05-12 01:35:12 +0300

    tests for addition

--- tests/BigNumber/Functions.spec.cpp
+++ tests/BigNumber/Functions.spec.cpp
@@ -5,14 +5,37 @@
 #include "gtest/gtest.h"
 #include "BigNumber/BigNumber.h"
 #include "BigNumber/Functions.h"
+#include <string>
+using namespace std;
+#include "gmp.h"
+
+
+void gen_big_number(mpz_t num) {
+    mpz_init(num);
+    unsigned long int i, seed;
+    gmp_randstate_t r_state;
+    seed = rand() % 10000;
+    gmp_randinit_default (r_state);
+    gmp_randseed_ui(r_state, seed);
+    mpz_urandomb(num, r_state, 264);
+}
+
 
 TEST(FunctionsAddition, POSITIVE) {
-    BigNumber a("0");
-    BigNumber b("0");
-    BigNumber c = addition(a, b);
-    EXPECT_EQ(c.print(), "444");
+    mpz_t a, b, res;
+    mpz_init(res);
+    gen_big_number(a);
+    gen_big_number(b);
+    mpz_add(res, a, b);
+    char *aSt, *bSt, *resSt;
+    mpz_get_str(aSt, 10, a);
+    mpz_get_str(bSt, 10, b);
+    mpz_get_str(resSt, 10, res);
+    string aStr(aSt), bStr(bSt), resStr(resSt);
+    EXPECT_EQ(resStr, sum(BigNumber(aStr), BigNumber(bStr)));
 }
 
 TEST(FunctionsAddition, NEGATIVE) {
-//    EXPECT_NE("123123", BigNumber("0").print());
+    
 }
+
