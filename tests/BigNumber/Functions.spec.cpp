68f06de7a38c98c56998e314891529a22b528c0c
Axel <axelgreenkp@gmail.com>
2016-05-24 14:24:20 +0300

    test tests

--- tests/BigNumber/Functions.spec.cpp
+++ tests/BigNumber/Functions.spec.cpp
@@ -9,33 +9,37 @@
 #include <string>
 using namespace std;
 #include "gmp.h"
+#include "chrono"
 
 
-void gen_big_number(mpz_t num) {
+void gen_big_number(mpz_t num, int digits = 256) {
     mpz_init(num);
-    unsigned long int seed;
+    unsigned int seed;
     gmp_randstate_t r_state;
-    seed = rand() % 10000;
-    gmp_randinit_default (r_state);
+    seed = rand() % 1000000;
+    srand(seed);
+    gmp_randinit_mt (r_state);
     gmp_randseed_ui(r_state, seed);
-    mpz_urandomb(num, r_state, 2);
-    gmp_printf("%d", num);
+    mpz_urandomb(num, r_state, digits);
+    gmp_randclear(r_state);
 }
 
 
 TEST(FunctionsAddition, POSITIVE) {
     mpz_t a, b, res;
     mpz_init(res);
-    gen_big_number(a);
-    gen_big_number(b);
+    gen_big_number(a, 1000000);
+    gen_big_number(b, 1000000);
     mpz_add(res, a, b);
-    char *aSt, *bSt, *resSt;
-    gmp_printf("%d \n", b);
-//    mpz_get_str(aSt, 10, a);
-//    mpz_get_str(bSt, 10, b);
-//    mpz_get_str(resSt, 10, res);
-//    string aStr(aSt), bStr(bSt), resStr(resSt);
-//    EXPECT_EQ(resStr, sum(BigNumber(aStr), BigNumber(bStr)).print());
+
+    char * aSt = mpz_get_str(NULL,10,a);
+    char * bSt = mpz_get_str(NULL,10,b);
+    char * resSt = mpz_get_str(NULL,10,res);
+    string aStr(aSt), bStr(bSt), resStr(resSt);
+    string sumSt;
+    sumSt = sum(BigNumber(aStr), BigNumber(bStr)).print();
+
+    EXPECT_EQ(resStr, sumSt);
 }
 
 TEST(FunctionsAddition, NEGATIVE) {
