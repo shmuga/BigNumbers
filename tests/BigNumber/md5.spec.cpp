9b5169d4e7609447fcf21df9e0dc795bcf097219
Mark <mail.ormark@gmail.com>
2016-06-04 10:34:46 +0300

    MD5 test

--- tests/BigNumber/md5.spec.cpp
+++ tests/BigNumber/md5.spec.cpp
@@ -1,4 +1,13 @@
 //
-// Created by Mark Orel on 6/4/16.
+// Created by Mark Orel on 6/3/16.
 //
 
+#include "gtest/gtest.h"
+#include "BigNumber/md5.h"
+
+using namespace std;
+
+TEST(MD5, HASH) {
+    cout <<  md5("hallo world") << endl;
+
+}
